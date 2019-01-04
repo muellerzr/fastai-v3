8f6acdbdb0617d75120d1120c0bd4331866bdfe4
Zachary Mueller <muellerzr@gmail.com>
2019-01-04 17:03:34 -0600

    Update server.py
    Uploaded correct model

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/wskasc12pmlicng/final_product.pth?dl=1'
-model_file_name = 'final_product'
+model_file_url = 'https://www.dropbox.com/s/4nncbwc4duxqtk0/test.pth?dl=1'
+model_file_name = 'test'
 classes = ['Lampropeltis elapsoides', 'Micrurus fulvius', 'Agkistrodon piscivorus','Coluber constrictor priapus', 'Drymarchon couperi', 'Nerodia fasciata','Pantherophis guttatus', 'Sistrurus miliarius barbouri']
 path = Path(__file__).parent
 
