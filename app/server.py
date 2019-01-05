716592bceed501e9a434bfe49a8ca7cf8636535d
Zachary Mueller <muellerzr@gmail.com>
2019-01-04 18:11:36 -0600

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,14 @@ from fastai.vision import *
 
 model_file_url = 'https://www.dropbox.com/s/ezs50e521qbagz5/test.pth?dl=1'
 model_file_name = 'test'
-classes = ['Lampropeltis elapsoides', 'Micrurus fulvius', 'Agkistrodon piscivorus','Coluber constrictor priapus', 'Drymarchon couperi', 'Nerodia fasciata','Pantherophis guttatus', 'Sistrurus miliarius barbouri']
+classes = ['Lampropeltis elapsoides',
+ 'Micrurus fulvius',
+ 'Agkistrodon piscivorus',
+ 'Coluber constrictor priapus',
+ 'Drymarchon couperi',
+ 'Nerodia fasciata',
+ 'Pantherophis guttatus',
+ 'Sistrurus miliarius barbouri']
 path = Path(__file__).parent
 
 app = Starlette()
